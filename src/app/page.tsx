37cbc74402a4431a350c5f364fb00597cacf1aa8
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2024-02-12 16:01:28 +0530

    Initial Repository Next TS, Tailwind CSS, ShadCN

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="bg-black "><Button variant="outline">Home</Button></main>
+  );
+}
